ff17a7bf8ad1c3ccf1baf1cc8ad7f154910c8902
¨JOHNSL98¨ <johnasuarezl@gmail.com>
2017-11-16 12:55:02 -0500

    se agrego operacion resta 2 - 3

--- index.java
+++ index.java
@@ -3,7 +3,11 @@ public class index{
 	public int Suma(int a, int b){
 		return a + b;
 	}
+	public int Resta(int a, int b){
+		return a - b;
+	}
 	public static void main(String []args){
 		system.out.println("Suma 2 + 3 = " + Suma(2, 3));
+		system.out.println("Resta 2 - 3 = " + Resta(2, 3));
 	}
 }
